36e915f7fcb41de75c24b387c91a764bd024d4bd
kevin <spamventallday@gmail.com>
2025-11-18 16:55:44 -0500

    initial commit

---
+++ modele.py
@@ -0,0 +1,155 @@
+import random
+
+# ------------------ CLASSES ------------------
+
+class Projectile:
+    def __init__(self, x, y, typeBullet):
+        self.x = x
+        self.y = y
+        self.taille_x = 2
+        self.taille_y = 10
+        self.vitesse = -10
+        self.goodBad = typeBullet # "g": ally bullet, "b": enemy bullet
+
+        if self.goodBad == "b":
+            self.vitesse = 10
+        
+    def typeProjectile(self):
+        self.vitesse = -10
+        if self.goodBad == "b":
+            self.vitesse *= -1
+
+    def mise_a_jour(self):
+        self.y += self.vitesse
+
+class Vaisseau:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.vie = 3
+        self.projectiles = []
+        self.taille_x = 15
+        self.taille_y = 15
+
+    def deplacer(self, x):
+        self.x = x
+
+    def tirer(self):
+        nouveau_proj = Projectile(self.x, self.y - 20, "g")
+        self.projectiles.append(nouveau_proj)
+
+    def mise_a_jour(self):
+        for p in self.projectiles:
+            p.mise_a_jour()
+
+        self.projectiles = [
+            p for p in self.projectiles
+            if p.y > 0 
+        ]
+
+
+class OVNI:
+    def __init__(self, x, y, vy):
+        self.x = x
+        self.y = y
+        self.vy = vy
+        self.projectiles = []
+        self.taille_x = 12
+        self.taille_y = 6
+
+    def tirer(self):
+        nouveau_proj = Projectile(self.x, self.y + 20, "b")
+        self.projectiles.append(nouveau_proj)
+
+    def mouvement_projectile(self):
+        for p in self.projectiles: 
+            p.mise_a_jour()
+
+    def mise_a_jour(self):
+        self.y += self.vy
+
+
+class Asteroide:
+    def __init__(self, x, y, vy):
+        self.x = x
+        self.y = y
+        self.vy = vy
+        self.taille_x = 10
+        self.taille_y = 10
+
+    def mise_a_jour(self):
+        self.y += self.vy
+
+
+# ------------------ MODÈLE ------------------
+
+class Modele:
+    def __init__(self, parent, largeur, hauteur):
+        self.parent = parent
+        self.largeur = 600
+        self.hauteur = 700
+        self.vaisseau = Vaisseau(self.largeur // 2, self.hauteur - 50)
+        self.ovnis = []
+        self.asteroides = []
+        self.score = 0
+        self.niveau = 1
+
+    def deplacer_vaisseau(self,x):
+        self.vaisseau.deplacer(x)
+    def tirer(self):
+        self.vaisseau.tirer()
+    def mise_a_jour(self):
+        self.vaisseau.mise_a_jour()
+        for p in self.vaisseau.projectiles:
+            if p.goodBad == "g":
+                for o in self.ovnis:
+                    if p.x <= o.x + o.taille_x / 2 and p.x >= o.x - o.taille_x / 2:
+                        if p.y - p.taille_y / 2 <= o.y + o.taille_y and not p.y + p.taille_y / 2 <= o.y - o.taille_y / 2:
+                            print("toucher", p.x, p.y)
+
+        # Apparition aléatoire des ennemis
+        alea_ovni = random.random()
+        if alea_ovni < 0.02:
+            nouvel_ovni = OVNI(
+                random.randint(0, self.largeur),
+                0,
+                random.randint(2, 5)
+            )
+            self.ovnis.append(nouvel_ovni)
+
+        alea_asteroide = random.random()
+        if alea_asteroide < 0.01:
+            nouvel_ast = Asteroide(
+                random.randint(0, self.largeur),
+                0,
+                random.randint(3, 6)
+            )
+            self.asteroides.append(nouvel_ast)
+
+        # Déplacement des ennemis
+        for o in self.ovnis:
+            o.mise_a_jour()
+
+        for a in self.asteroides:
+            a.mise_a_jour()
+
+        # Les ennemis tirent
+        for o in self.ovnis:
+            alea_frequence = random.random()
+            if alea_frequence < 0.02:
+                o.tirer()
+                print("ovni tire")
+
+        for o in self.ovnis:
+            o.mouvement_projectile()
+
+        # Nettoyage des objets sortis de l'écran (À COMPLETER)
+        self.ovnis = [
+            o for o in self.ovnis
+            if o.y < self.hauteur
+        ] 
+
+        self.asteroides = [
+            a for a in self.asteroides
+            if a.y < self.hauteur
+        ]
